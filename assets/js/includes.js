52e2ae50c07d6cf31932dda2400363a77185aaa3
gerard lucas <gerardlucasy2j@gmail.com>
2026-03-17 15:11:19 +0530

    after forms

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -21,11 +21,23 @@
             if (xhr.status === 200 && xhr.responseText && xhr.responseText.length > 100) {
                 headerEl.outerHTML = xhr.responseText;
                 var script = document.getElementById('header-mobile-script');
-                if (script) {
-                    var s = document.createElement('script');
-                    s.textContent = script.textContent;
-                    document.body.appendChild(s);
-                    script.remove();
+                var scriptContent = script ? script.textContent : '';
+                if (script) script.remove();
+                function runHeaderScript() {
+                    if (scriptContent) {
+                        var s = document.createElement('script');
+                        s.textContent = scriptContent;
+                        document.body.appendChild(s);
+                    }
+                }
+                if (typeof Swal !== 'undefined') {
+                    runHeaderScript();
+                } else {
+                    var loadSwal = document.createElement('script');
+                    loadSwal.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@11';
+                    loadSwal.onload = runHeaderScript;
+                    loadSwal.onerror = runHeaderScript;
+                    document.head.appendChild(loadSwal);
                 }
             } else {
                 headerEl.outerHTML = '<header class="header-section header-section-3" style="display:block;visibility:visible;"><div class="topbar-area"><div class="container"><div class="row"><div class="col-lg-6"><div class="topbar-contact"><a href="tel:+919902758811"><i class="fa fa-phone"></i> +91 99027 58811</a><a href="mailto:info@cysacademy.in" class="margin-left"><i class="fa fa-envelope"></i> info@cysacademy.in</a></div></div><div class="col-lg-6"><div class="topbar-button"><a href="contact.html"><i class="fa fa-sign-in"></i> Enquire Now</a><a href="assets/docs/brochure.pdf" class="margin-left" target="_blank" rel="noopener noreferrer" download><i class="fa fa-download"></i> Download Brochure</a></div></div></div></div></div><div class="menu-area"><div class="container"><div class="row align-items-center"><div class="col-lg-2 col-6"><div class="logo-area"><a href="index.html"><img src="assets/images/logo.png" alt="CYS Academy"></a></div></div><div class="col-lg-10 d-none d-lg-block"><nav><ul class="main-menu"><li><a href="index.html">Home</a></li><li><a href="aboutus.html">About Us</a></li><li><a href="courses.html">Courses</a></li><li><a href="blog.html">Blog</a></li><li><a href="contact.html">Contact Us</a></li><li><a href="contact.html">Get Started</a></li></ul></nav></div></div></div></div></header>';
